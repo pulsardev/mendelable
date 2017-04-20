7cbd4a9d88713fe9b2f45b6bdf1dd1dd9870fa95
Mathieu Morainville <mathieumorainville@hotmail.com>
2017-04-20 20:41:32 +0200

    style: add Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!--<img src="./assets/logo.png">-->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import './assets/scss/design.scss';
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+window.$ = window.jQuery = require('jquery')
+window.tether = window.Tether = require('tether')
+require('bootstrap')
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
