2f461fa4fd0933ce7bd451d96f0270dba52f1050
Mathieu Morainville <mathieumorainville@hotmail.com>
2017-05-05 20:29:36 +0200

    feat: add buttons to go to the next or the previous element

--- src/components/Sidebar/ElementProfileOptions.vue
+++ src/components/Sidebar/ElementProfileOptions.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="c-element-profile-options">
     <div class="btn-group w-100" role="group" aria-label="Navigation">
-      <router-link :to="'/element/' + previousElementId" class="btn btn-primary w-100">{{ $t("sidebar.elementProfile.previous") }}</router-link>
-      <router-link :to="'/element/' + nextElementId" class="btn btn-primary w-100">{{ $t("sidebar.elementProfile.next") }}</router-link>
+      <router-link :to="'/element/' + previousElementId" tag="button" type="button" class="btn btn-primary w-100" :disabled="previousElementId === 0">{{ $t("sidebar.elementProfile.previous") }}</router-link>
+      <router-link :to="'/element/' + nextElementId" tag="button" type="button" class="btn btn-primary w-100" :disabled="nextElementId === numberOfElements + 1">{{ $t("sidebar.elementProfile.next") }}</router-link>
     </div>
   </div>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+
   export default {
     name: 'element-profile-options',
     computed: {
+      ...mapState({
+        numberOfElements: state => Object.keys(state.elements.default).length
+      }),
       previousElementId () {
         return +this.$route.params.id - 1
       },
