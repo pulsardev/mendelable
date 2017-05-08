936b1aaff637a2c392e06b24676f87c300c727d3
Mathieu Morainville <mathieumorainville@hotmail.com>
2017-05-08 02:09:12 +0200

    fix: contact form layout and translations

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,38 +1,32 @@
 <template>
   <div class="c-about">
-    <h2>About Mendelable</h2>
-    <div class ="col-6">
-      <p>
-        Mendelable is a free open-source project hosted by Github and developped by the team Plusar which you can contribute through this form.
-        You can conctact us if you find a problem in our datasets, if you have a technical issue or if you have just a simple question.
-      </p>
-    </div>
+    <h2>{{ $t("about.title") }}</h2>
+    <p class="text-muted" v-html="$t('about.primaryText')"></p>
+
     <div class="row">
-      <div class="col-lg-12">
+      <div class="col-xl-8">
         <form action="https://formspree.io/teampulsar.dev@gmail.com" method="POST" name="sentMessage" id="contactForm" novalidate>
-          <div class="row">
-            <div class="col-md-6">
-              <div class="form-group">
-                <input v-model="name" type="text" name="name" class="form-control" placeholder="Your Name *" id="name" required data-validation-required-message="Please enter your name.">
-                <p class="help-block text-danger"></p>
-              </div>
-              <div class="form-group">
-                <input v-model="email" type="email" name="_replyto" class="form-control" placeholder="Your Email *" id="email" required data-validation-required-message="Please enter your email address.">
-                <p v-if="email && !isValidEmail" class="help-block text-danger">Please enter a valid email address.</p>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="form-group">
-                <textarea v-model="message" name="message" class="form-control" placeholder="Your Message *" id="message" required data-validation-required-message="Please enter a message."></textarea>
-                <p class="help-block text-danger"></p>
-              </div>
-            </div>
-            <div class="clearfix"></div>
-            <div class="col-lg-12 text-center">
-              <div id="success"></div>
-              <button type="submit" class="btn btn-xl" :disabled="!canSubmit()">Send Message</button>
-            </div>
+
+          <div class="form-group">
+            <label for="name">{{ $t('contact.form.name.label') }}</label>
+            <input v-model="name" type="text" name="name" class="form-control" :placeholder="$t('contact.form.name.label') + '*'" id="name" required :data-validation-required-message="$t('contact.form.name.validation')">
+            <p class="help-block text-danger"></p>
           </div>
+
+          <div class="form-group">
+            <label for="email">{{ $t('contact.form.email.label') }}</label>
+            <input v-model="email" type="email" name="_replyto" class="form-control" :placeholder="$t('contact.form.email.label') + '*'" id="email" required :data-validation-required-message="$t('contact.form.email.validation')">
+            <small id="emailHelp" class="form-text text-muted">{{ $t('contact.form.email.helpText') }}</small>
+            <p v-if="email && !isValidEmail" class="help-block text-danger">{{ $t('contact.form.email.error') }}</p>
+          </div>
+
+          <div class="form-group">
+            <label for="message">{{ $t('contact.form.message.label') }}</label>
+            <textarea v-model="message" name="message" class="form-control" :placeholder="$t('contact.form.message.label') + '*'" id="message" rows="5" required :data-validation-required-message="$t('contact.form.message.validation')"></textarea>
+            <p class="help-block text-danger"></p>
+          </div>
+
+          <button type="submit" class="btn btn-primary" :disabled="!canSubmit()">{{ $t('contact.form.submit') }}</button>
         </form>
       </div>
     </div>
