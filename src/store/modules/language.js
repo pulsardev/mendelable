e752276cee43544fdbdd2abe25cad212ad0ca59d
Mathieu Morainville <mathieumorainville@hotmail.com>
2017-05-21 16:21:11 +0200

    fix(translation): fetch the language from the navigator's property

--- src/store/modules/language.js
+++ src/store/modules/language.js
@@ -1,7 +1,20 @@
 import * as types from '../mutation-types'
 
+let getNavigatorLanguage = function () {
+  console.log('navigator.language', window.navigator.language)
+  let language = window.navigator.language.toLowerCase().replace('-', '_')
+
+  switch (language) {
+    case 'fr':
+    case 'fr_fr':
+      return 'fr_FR'
+    default:
+      return 'en_US'
+  }
+}
+
 const state = {
-  locale: localStorage.getItem('locale') ? localStorage.getItem('locale') : 'fr_FR',
+  locale: localStorage.getItem('locale') ? localStorage.getItem('locale') : getNavigatorLanguage(),
   fallbackLocale: 'en_US'
 }
 
